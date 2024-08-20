488462ee58d6f47ac6e3ff324789f359998cbe86
rajiv <rajivnatrajan20@gmail.com>
2024-08-20 16:54:11 +0530

    fourthpgm

---
+++ src/main/scala/Divisblby5n7.scala
@@ -0,0 +1,12 @@
+object Divisblby5n7 {
+  def Divisbl5r7(x:Int):Boolean={
+    x % 5 == 0 && x % 7 == 0
+      }
+  def main(args:Array[String]): Unit = {
+    println(Divisbl5r7(45))
+
+  }
+}
+
+
+
